4f49420ecbf673bce5381c7078889f8fb1e9f456
Priya Chouhan <pc2315168@gmail.com>
2025-01-04 18:33:49 +0530

    Revert "One"

--- script.js
+++
@@ -1,8 +0,0 @@
-const h = document.getElementById('heading');
-let btn = document.getElementById('btn');
-
-btn.addEventListener("click", () =>{
-    let body = document.body;
-    body.style.background = "red";
-    body.style.color = "black";
-})
