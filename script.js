86407a32a833da9a7c7d1699e3b346bca5b235f1
Priya Chouhan <priyachouhan23@navgurukul.org>
2025-01-04 18:28:01 +0530

    added color in background

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ let btn = document.getElementById('btn');
 btn.addEventListener("click", () =>{
     let body = document.body;
     body.style.background = "red";
+    body.style.color = "black";
 })
